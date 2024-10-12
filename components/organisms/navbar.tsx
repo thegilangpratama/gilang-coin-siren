7dfd55507803c09d23b84604dfab183a80e70941
Gilang Pratama <69042498+thegilangpratama@users.noreply.github.com>
2024-10-12 13:38:25 +0700

    api implementation and ssr

--- components/organisms/navbar.tsx
+++ components/organisms/navbar.tsx
@@ -3,6 +3,21 @@ import React from 'react';
 import Image from 'next/image';
 import { Button } from '@/components/atoms/button';
 
+import {
+    DropdownMenu,
+    DropdownMenuContent,
+    DropdownMenuItem,
+    DropdownMenuLabel,
+    DropdownMenuSeparator,
+    DropdownMenuTrigger,
+} from "@/components/atoms/dropdown-menu"
+
+import {
+    ChevronDownIcon
+} from "@radix-ui/react-icons"
+
+
+
 const Navbar: React.FC = () => {
     return (
         <div className={"top-0 w-full flex justify-center bg-transparent h-16 md:h-20 z-50 transition-all"}>
@@ -18,8 +33,24 @@ const Navbar: React.FC = () => {
                 </div>
                 <div className='hidden md:flex space-x-10 '>
                     <div>
-                        채용
-
+                        <DropdownMenu>
+                            <DropdownMenuTrigger>
+                                <div className='flex space-x-2'>
+                                    <p>
+                                        채용
+                                    </p>
+                                    <ChevronDownIcon />
+                                </div>
+                            </DropdownMenuTrigger>
+                            <DropdownMenuContent>
+                                <DropdownMenuLabel>채용</DropdownMenuLabel>
+                                <DropdownMenuSeparator />
+                                <DropdownMenuItem>해외 개발자 원격 채용</DropdownMenuItem>
+                                <DropdownMenuItem>외국인 원격 채용 (비개발 직군)</DropdownMenuItem>
+                                <DropdownMenuSeparator />
+                                <DropdownMenuItem>한국어 가능 외국인 채용</DropdownMenuItem>
+                            </DropdownMenuContent>
+                        </DropdownMenu>
                     </div>
                     <div>
                         해외 개발자 활용 서비스
--- tailwind.config.ts
+++ tailwind.config.ts
@@ -56,7 +56,27 @@ const config: Config = {
 				lg: 'var(--radius)',
 				md: 'calc(var(--radius) - 2px)',
 				sm: 'calc(var(--radius) - 4px)'
-			}
+			},
+			keyframes: {
+				fadeInUp: {
+					'0%': {
+						opacity: '0',
+						transform: 'translateY(20px)',
+					},
+					'100%': {
+						opacity: '1',
+						transform: 'translateY(0)',
+					},
+				},
+				fade: {
+					'0%': { opacity: '0' },
+					'100%': { opacity: '1' },
+				},
+			},
+			animation: {
+				fadeInUp: 'fadeInUp 0.5s ease-out',
+				fade: 'fade 500ms ease-in-out',
+			},
 		}
 	},
 	plugins: [require("tailwindcss-animate")],
